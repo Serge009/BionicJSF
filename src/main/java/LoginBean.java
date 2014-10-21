ba171da71fbd1a809065599221c551300575d8b2
serge009 <serge.naumovych@gmail.com>
2014-10-21 20:40:49 +0300

    change LoginBean

--- src/main/java/LoginBean.java
+++ src/main/java/LoginBean.java
@@ -1,5 +1,7 @@
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 /**
  * Created by oper4 on 21.10.2014.
@@ -24,5 +26,16 @@ public class LoginBean {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void saveMessage() {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        context.addMessage(null, new FacesMessage("Second Message", "Additional Message Detail"));
+    }
+
+    public void save() {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Welcome " + name));
     }
 }
